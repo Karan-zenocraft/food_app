878332fb06090fba203ade2eabb84035aa746427
root <root@localhost.creatusello.com>
2019-12-17 19:54:41 +0530

    add api related changes

--- backend/config/main.php
+++ backend/config/main.php
@@ -41,12 +41,12 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'scriptUrl' => ($_SERVER['HTTP_HOST'] == "localhost") ? '/food_app/admin' : '/admin',
+            'scriptUrl' => ($_SERVER['HTTP_HOST'] == "localhost") ? '/food_app/admin' : '/food_app/admin',
             'rules' => [
             ],
         ],
         'request' => [
-            'baseUrl' => ($_SERVER['HTTP_HOST'] == "localhost") ? '/food_app/admin' : '/admin',
+            'baseUrl' => ($_SERVER['HTTP_HOST'] == "localhost") ? '/food_app/admin' : '/food_app/admin',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'pbB0NvlmxlWRk7XFCN_7XUC2uvX0vyCD',
         ],
